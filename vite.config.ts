56f0706b48497d178a6e629256fa45e8dc66ac7c
Eriqo90AW <eriqo.arief@ui.ac.id>
2026-05-14 14:28:02 +0700

    v1.83

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin(), tailwindcss()],
+  optimizeDeps: {
+    include: ["solid-js", "solid-js/store", "@solid-devtools/shared"],
+  },
   server: {
     port: 3077,
-    host: '0.0.0.0',
-    allowedHosts: ['finlyzen.ercloud.site'],
+    host: "0.0.0.0",
+    allowedHosts: ["finlyzen.ercloud.site"],
+    hmr: {
+      clientPort: 443,
+    },
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
   preview: {
-    host: '0.0.0.0',
-    allowedHosts: ['finlyzen.ercloud.site'],
-  }
+    host: "0.0.0.0",
+    allowedHosts: ["finlyzen.ercloud.site"],
+  },
 });
